b340d442a9d89bb449220fa658b40897a8377f05
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-04 09:18:24 -0500

    [ADD]: envitonment variables + mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AuthModule } from './auth/auth.module';
+import { EnvConfiguration } from './config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      isGlobal: true, //Con esto evitamos tener q importar el 'ConfigModule' en c/module q utilize EnvV
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
