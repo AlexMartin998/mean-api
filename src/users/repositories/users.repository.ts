f3388eeaa1efea00ed6a0d5ec8658f81dcfc759a
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-04 22:26:59 -0500

    [ADD]: find user by id and validate its status (soft delete)

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -5,7 +5,7 @@ import {
   Logger,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 
 import { RegisterDto } from 'src/auth/dto';
 import { User } from '../entities/user.entity';
@@ -28,8 +28,11 @@ export class UserRepository {
     }
   }
 
-  async findOne(id: number): Promise<User> {
-    return this.userModel.findById(id);
+  async findOne(id: string): Promise<User | null> {
+    if (!isValidObjectId(id)) throw new BadRequestException('Invalid User ID');
+
+    // validate soft delete
+    return this.userModel.findById(id).where('isDeleted', false);
   }
 
   async findOneByEmail(email: string): Promise<User> {
