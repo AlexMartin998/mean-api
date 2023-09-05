77d30a2f9619b02cbf40384e8e2c56ea6c141f04
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-04 20:45:10 -0500

    [ADD]: AuthGuard and initial JWT validation

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Envs } from 'src/common/constants';
+import { JwtPayload } from '../common/interfaces';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) throw new UnauthorizedException();
+
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(token, {
+        secret: this.configService.get<string>(Envs.JWT_SECRET),
+      });
+      request['userId'] = payload.id;
+
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
+
+// https://docs.nestjs.com/security/authentication#implementing-the-authentication-guard
